428aeabd2b48e4c8c4c0215002b1a5f5d4aef8ff
Vitaly Svyatyuk <goodgame1945@gmail.com>
2016-12-10 16:21:15 +0300

    main page is ok

--- category/catelist/forms.py
+++ category/catelist/forms.py
@@ -2,7 +2,8 @@ from django import forms
 
 
 class CategoryForm(forms.Form):
-	name = forms.CharField(label='Category Name', max_length=50)
+	name = forms.CharField(label='Category Name:', max_length=50,
+							widget=forms.TextInput(attrs={'placeholder': ' '}))
 
 
 class EditCategoryForm(forms.Form):
--- category/catelist/views.py
+++ category/catelist/views.py
@@ -6,13 +6,38 @@ from .forms import CategoryForm, EditCategoryForm
 
 
 def categories(request):
-    categories = Category.objects.all()
-    form = CategoryForm()
-    return render(request, 'categories.html', 
-    			 {'nodes': categories, 'form': form})
+    if request.method == 'POST':
+        already_exists = False
+        name = request.POST['name']
+        categories = Category.objects.all() 
+        for catname in categories:
+            if str(catname) == str(name):
+                form = CategoryForm(request.POST)
+                already_exists = True
+                return render(request, 'categories.html', 
+                             {'categories': categories, 
+                              'form': form,
+                              'already_exists': already_exists})  
+        parent = request.POST['select-parent']
+        if parent == "0":
+            cat = Category(name=name)
+            cat.save()
+        else:
+            parent = Category.objects.get(id=parent)
+            cat = Category(name=name, parent=parent)
+            cat.save()
+        categories = Category.objects.all()
+        form = CategoryForm()
+        return render(request, 'categories.html', 
+                     {'categories': categories, 'form': form})
+    else:
+        categories = Category.objects.all()
+        form = CategoryForm()
+        return render(request, 'categories.html', 
+                     {'categories': categories, 'form': form})
   
 def category(request, category_id):
     category = Category.objects.get(pk=category_id)
     form = EditCategoryForm()
     return render(request, 'category.html', 
-    	         {'category': category, 'form': form })
+                 {'category': category, 'form': form })
